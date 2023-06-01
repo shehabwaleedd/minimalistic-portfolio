a9eee182e13227b8de992acd1e5d26213df227e7
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-01 12:32:55 +0300

    70% responsive

--- src/pages/home/main/Main.jsx
+++ src/pages/home/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react'
+import React, { useEffect } from 'react'
 import "./Main.css"
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
@@ -12,45 +12,45 @@ const Main = (Props) => {
     useEffect(() => {
 
         const helloWorld = document.querySelector('.main__content');
-    
+
 
         function handleDoubleClick() {
             helloWorld.classList.toggle('selected');
-          }
-      
-          helloWorld.addEventListener('dblclick', handleDoubleClick);
-      
-          // Clean up the event listener on unmount
-          return () => {
+        }
+
+        helloWorld.addEventListener('dblclick', handleDoubleClick);
+
+        // Clean up the event listener on unmount
+        return () => {
             helloWorld.removeEventListener('dblclick', handleDoubleClick);
-          };
+        };
 
 
-      }, []);
+    }, []);
 
 
 
-  return (
-    <section className='main section'>
-        <div className="main__container container grid">
-            <div className="main__content" style={{ marginTop: Props.navOpen ? "5rem" : "0" }}>
-                <h1 className="main__titlee">HEY, {t("my__name_im")} <span className="main__title-color">{t("my__name-first")}</span> <span> {t("my__name-last")}</span></h1>
-                <h1 className="main__subtitle">{t("my__intro")}</h1>
-            </div>
+    return (
+        <section className='main section'>
+            <div className="main__container container grid">
+                <div className="main__content" style={{ marginTop: Props.navOpen ? "5rem" : "0" }}>
+                    <h1 className="main__titlee">HEY, {t("my__name_im")} <span className="main__title-color">{t("my__name-first")}</span> <span> {t("my__name-last")}</span></h1>
+                    <h1 className="main__subtitle">{t("my__intro")}</h1>
+                </div>
                 <div className="main__subcontent">
                     <p className="main__description">{t("Introduction")}</p>
                 </div>
-            <div className="main__links">
-                <Link to='/portfolio' className="main__button button1">
-                    <p><i className='bx bx-right-arrow-alt'></i> {t("button__view_my_projects")} </p>
-                </Link>
-                <Link to='/about' className="main__button button2">
-                    <p><i className='bx bx-right-arrow-alt'></i> {t("button__more__about_me")}</p>
-                </Link>
+                <div className="main__links">
+                    <Link to='/portfolio' className="main__button button1">
+                        <p><i className='bx bx-right-arrow-alt'></i> {t("button__view_my_projects")} </p>
+                    </Link>
+                    <Link to='/about' className="main__button button2">
+                        <p><i className='bx bx-right-arrow-alt'></i> {t("button__more__about_me")}</p>
+                    </Link>
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Main
