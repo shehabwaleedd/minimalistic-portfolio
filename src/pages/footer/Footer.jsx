98a9ba21d84ab9b9dc6421f47341897d9c64b77e
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-24 13:16:17 +0400

    made the footer responsive for small screens

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -12,13 +12,13 @@ const Footer = (Props) => {
           <h1 style={{ fontFamily: Props.language === "ar" ? "Aref Ruqaa" : "", letterSpacing: Props.language === "ar" ? "0" : "0.3rem", fontSize: Props.language === "ar" ? "4rem" : "1.7rem" }} className="footer__title">{t("my__name")} {t("my__name-last")}</h1>
           <div className="footer__social">
             <div className="footer__info" style={{ flexDirection: Props.language === "ar" ? "row-reverse" : "row", gap: Props.language === "ar" ? "1rem" : "0.2rem", }}>
-              <p style={{fontSize: "0.8rem" }}><strong id="mono">{t("footer__info-address")}</strong>{t("footer__info-street")}, {t("footer__info-egypt")}</p>
+              <p style={{fontSize: "0.7rem" }}><strong id="mono" style={{fontSize: "0.7rem" }}>{t("footer__info-address")}</strong>{t("footer__info-street")}, {t("footer__info-egypt")}</p>
             </div>
             <div className="footer__info">
-              <p><strong id="mono">{t("footer__info-freelancing")}</strong>   {t("footer__info_hours")} {t("footer__info-mon-to-sat")}</p>
+              <p style={{fontSize: "0.7rem" }}><strong id="mono" style={{fontSize: "0.7rem" }}>{t("footer__info-freelancing")}</strong>   {t("footer__info_hours")} {t("footer__info-mon-to-sat")}</p>
             </div>
             <div className="footer__info">
-              <p><strong id="mono">{t("footer__info-phone")}</strong>{t("footer__info_number")}</p>
+              <p style={{fontSize: "0.7rem" }}><strong id="mono" style={{fontSize: "0.7rem" }}>{t("footer__info-phone")}</strong>{t("footer__info_number")}</p>
             </div>
             <div className="footer__socials">
               <a href="https://www.instagram.com/shehabwaleedd/" className="footer__social-link" target='_blank' rel="noreferrer"><i className="uil uil-instagram"></i></a>
