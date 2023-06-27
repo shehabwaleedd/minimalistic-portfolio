1ccd3a81492796af6ab55cc6d2d6d0d32838745d
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-28 02:30:12 +0400

    changed the postitioning of the main content

--- src/animation/HomeAnimations.js
+++ src/animation/HomeAnimations.js
@@ -81,7 +81,7 @@ export const HomeAnimations = () => {
 
   const animateOtherElements = () => {
     gsap.to(mainContainer, {
-      y: window.innerWidth > 1440 ? -250 : -150,
+      y: window.innerWidth > 1440 ? -200 : -100,
       opacity: 1,
       ease: "expo.inOut",
       duration: 3,
