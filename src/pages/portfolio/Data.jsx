b563f9970cefc79a1c88156a31b9b8cca86bcb3f
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-29 19:54:35 +0400

    updated moaz askaar poster

--- src/pages/portfolio/Data.jsx
+++ src/pages/portfolio/Data.jsx
@@ -1,7 +1,7 @@
 import Work0 from "../../assets/Posters/shebo2.png"
 import Work1 from "../../assets/Posters/lelu10.svg"
 import Work2 from "../../assets/Posters/blog1.svg"
-import Work3 from "../../assets/Posters/moaz.svg"
+import Work3 from "../../assets/Posters/moaz2.svg"
 import Work4 from "../../assets/Posters/cairo1.svg"
 
 const projectsData = [
