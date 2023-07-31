31f17438558060dcdab816834a0d9b5a141b6428
Shehab Waleed <shehabwaleedd@gmail.com>
2023-07-31 16:54:05 +0400

    redesigning the about page to make it more modern and to reflect my identity and vision.

--- src/pages/about/aboutResponsive/desktopAbout/DesktopAbout.jsx
+++ src/pages/about/aboutResponsive/desktopAbout/DesktopAbout.jsx
@@ -9,29 +9,32 @@ import cv from "../../../../assets/Shehab Waleed Resume.docx"
 const DesktopAbout = ({ language }) => {
     const { t } = useTranslation()
     return (
-        <motion.div >
-            <motion.div className="about__pic" >
-                <motion.img src={require("../../../../assets/profile.jpg")} alt="profile" />
-            </motion.div>
-            <motion.div className='detailed__containerr'>
-                <motion.div className='detailed__row' >
-                    <div className="aboutttt__container containered">
-                        <div className="about__content" style={{ marginTop: language === "fr" ? "10rem" : language === "de" || language === "en" ? "30rem" : "20rem" }}>
-                            <h1 style={{ textAlign: language === "ar" ? "right" : "left", paddingRight: language === "ar" ? "3rem" : "0", fontFamily: language === "ar" ? "Aref Ruqaa" : "", letterSpacing: language === "ar" ? "0" : "0.2rem", fontSize: language === "fr" ? "2rem" : "3rem" }}>{t("navbar__about")}</h1>
-                            <p style={{ fontSize: language === "fr" ? "0.7rem" : "0.7rem", fontFamily: language === "ar" ? "Almarai" : "", textAlignLast: language === "ar" ? "right" : "left" }}>
-                                {t("about__description")}
-                            </p>
-                            <div className="cv__button" style={{ justifyContent: language === "ar" ? "flex-end" : "flex-start", paddingRight: language === "ar" ? "19rem" : "0rem" }}>
-                                <button>
-                                    <BiDownArrowAlt className='scroll-down' />
-                                    <a href={cv} download>{t("about__resume")}</a>
-                                </button>
+        <motion.div className='about'>
+                <motion.div className="about__pic" >
+                    <motion.img src={require("../../../../assets/profile3.JPG")} alt="profile" />
+                </motion.div>
+                <motion.div className='detailed__containerr'>
+                    <motion.div className='detailed__row' >
+                        <div className="aboutttt__container container">
+                            <div className="about__content" style={{ marginTop: language === "fr" ? "10rem" : language === "de" || language === "en" ? "30rem" : "20rem" }}>
+                                <h1 style={{ textAlign: language === "ar" ? "right" : "left", paddingRight: language === "ar" ? "3rem" : "0", fontFamily: language === "ar" ? "Aref Ruqaa" : "", letterSpacing: language === "ar" ? "0" : "0.2rem", fontSize: language === "fr" ? "2rem" : "8.2rem" }}>{t("navbar__about")}</h1>
+                                <h2>
+                                I'm Shehab. A web developer, desginer and problem solver.
+                                </h2>
+                                <p style={{ fontSize: language === "fr" ? "1.1rem" : "1.1rem", fontFamily: language === "ar" ? "Almarai" : "", textAlignLast: language === "ar" ? "right" : "left" }}>
+                                    {t("about__description")}
+                                </p>
+                                <div className="cv__button" style={{ justifyContent: language === "ar" ? "flex-end" : "flex-start", paddingRight: language === "ar" ? "19rem" : "0rem" }}>
+                                    <button>
+                                        <BiDownArrowAlt className='scroll-down' />
+                                        <a href={cv} download>{t("about__resume")}</a>
+                                    </button>
+                                </div>
+            <MyStack />
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 </motion.div>
-            </motion.div>
-            <MyStack />
         </motion.div>
     )
 }
--- src/pages/about/stack/MyStack.jsx
+++ src/pages/about/stack/MyStack.jsx
@@ -19,14 +19,14 @@ const MyStack = (Props) => {
         <motion.div className='stack section'  initial={{ opacity: 0, y: -100 }} animate={{ opacity: 1, y:0, transition: { delay: 0.7, duration: 2, ease: "easeInOut" }}}  exit={{ opacity: 0, transition: { delay: 0.4, duration: 1, ease: "easeInOut" } }}>
           <motion.div className="stack__container container"  initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.7, duration: 2, ease: "easeInOut" }}}  exit={{ opacity: 0, transition: { delay: 0.4, duration: 1} }}>
             <div className="stack__icons">
-              <BiLogoJavascript style={{ color: "var(--stack-accent-color)" }} />
-              <BiLogoReact style={{ color: "var(--stack-accent-color)" }}/>
-              <BiLogoTailwindCss style={{ color: "var(--stack-accent-color)" }}/>
-              <BiLogoBootstrap style={{ color: "var(--stack-accent-color)" }}/>
-              <BiLogoSass style={{ color: "var(--stack-accent-color)" }}/>
-              <BiLogoFirebase style={{ color: "var(--stack-accent-color)" }}/>
-              <BiLogoGithub style={{ color: "var(--stack-accent-color)" }}/>
-              <BiLogoTypescript style={{ color: "var(--stack-accent-color)" }}/>
+              <BiLogoJavascript style={{ color: "var(--nav-overlay-color)" }} />
+              <BiLogoReact style={{ color: "var(--nav-overlay-color)" }}/>
+              <BiLogoTailwindCss style={{ color: "var(--nav-overlay-color)" }}/>
+              <BiLogoBootstrap style={{ color: "var(--nav-overlay-color)" }}/>
+              <BiLogoSass style={{ color: "var(--nav-overlay-color)" }}/>
+              <BiLogoFirebase style={{ color: "var(--nav-overlay-color)" }}/>
+              <BiLogoGithub style={{ color: "var(--nav-overlay-color)" }}/>
+              <BiLogoTypescript style={{ color: "var(--nav-overlay-color)" }}/>
             </div>
           </motion.div>
         </motion.div>
