4a25d7d13b9f2f65c54a2f6152fa145363bfbb44
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-01 15:30:51 +0400

    added while in view animation to desktop about page.

--- src/pages/about/aboutResponsive/desktopAbout/DesktopAbout.jsx
+++ src/pages/about/aboutResponsive/desktopAbout/DesktopAbout.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { motion } from 'framer-motion'
 import { useTranslation } from 'react-i18next'
-import { BiDownArrowAlt } from 'react-icons/bi'
 import MyStack from '../../stack/MyStack'
 import cv from "../../../../assets/Shehab Waleed Resume.docx"
 import "./DesktopAbout.scss"
 
 const DesktopAbout = ({ language }) => {
+
+    const aboutRef = useRef()
     const { t } = useTranslation()
     return (
-        <motion.div className='about_desktop'>
+        <motion.div className='about_desktop' ref={aboutRef}>
             <motion.div className="about__pic" >
                 <motion.img src={require("../../../../assets/profile3.JPG")} alt="profile" />
             </motion.div>
-            <motion.div className='detailed__containerr'>
+            <motion.div className='detailed__containerr' >
                 <motion.div className='detailed__row' >
                     <div className="aboutttt__container container">
                         <div className="about__content" style={{ marginTop: language === "fr" ? "10rem" : language === "de" || language === "en" ? "30rem" : "20rem" }}>
-                            <h1 style={{ textAlign: language === "ar" ? "right" : "left", paddingRight: language === "ar" ? "3rem" : "0", fontFamily: language === "ar" ? "Aref Ruqaa" : "", letterSpacing: language === "ar" ? "0" : "0.2rem", fontSize: language === "fr" ? "2rem" : "8.2rem" }}>{t("navbar__about")}</h1>
-                            <h2>I'm Shehab. A web developer, desginer and problem solver.</h2>
+                            <motion.h1 style={{ textAlign: language === "ar" ? "right" : "left", paddingRight: language === "ar" ? "3rem" : "0", fontFamily: language === "ar" ? "Aref Ruqaa" : "", letterSpacing: language === "ar" ? "0" : "0.2rem", fontSize: language === "fr" ? "2rem" : "8.2rem" }} initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>{t("navbar__about")}</motion.h1>
+                            <motion.h2 initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>I'm Shehab. A web developer, desginer and problem solver.</motion.h2>
                             <div className="about__description">
-                                <p style={{ fontSize: language === "fr" ? "1.1rem" : "1.1rem", fontFamily: language === "ar" ? "Almarai" : "", textAlignLast: language === "ar" ? "right" : "left" }}>
+                                <motion.p style={{ fontSize: language === "fr" ? "1.1rem" : "1.1rem", fontFamily: language === "ar" ? "Almarai" : "", textAlignLast: language === "ar" ? "right" : "left" }} initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>
                                     {t("about__description")}
-                                </p>
-                                <p style={{ fontSize: language === "fr" ? "1.1rem" : "1.1rem", fontFamily: language === "ar" ? "Almarai" : "", textAlignLast: language === "ar" ? "right" : "left" }}>
+                                </motion.p>
+                                <motion.p style={{ fontSize: language === "fr" ? "1.1rem" : "1.1rem", fontFamily: language === "ar" ? "Almarai" : "", textAlignLast: language === "ar" ? "right" : "left" }} initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>
                                     {t("about__description1")}
-                                </p>
+                                </motion.p>
                             </div>
                             <div className="about__quote">
-                                <span>One of my favourite quotes (by Marcus Aurelius):</span>
-                                <h1>“Give yourself a gift, the present moment.”</h1>
-                                <div className="quote__explain">
+                                <motion.span initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>One of my favourite quotes (by Marcus Aurelius):</motion.span>
+                                <motion.h1 initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>“Give yourself a gift, the present moment.”</motion.h1>
+                                <motion.div className="quote__explain" initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>
                                     <span>{t("about__quote_exp")}</span>
-                                </div>
+                                </motion.div>
                             </div>
                             <div className="cv__button" style={{ justifyContent: language === "ar" ? "flex-end" : "flex-start", paddingRight: language === "ar" ? "19rem" : "0rem" }}>
                                 <button>
@@ -44,27 +45,27 @@ const DesktopAbout = ({ language }) => {
                 </motion.div>
             </motion.div>
             <div className="about__bottom container">
-                <h1>{t("about__experience")}</h1>
-                <div className="about__bottom_container">
+                <motion.h1 initial={{ x: 50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>{t("about__experience")}</motion.h1>
+                <motion.div className="about__bottom_container" initial={{ x: 50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>
                     <div className="exp_desc">
                         <h2>- Degree in Financial Accounting</h2>
                         <h2>- a year and a half of experience in Frontend Development.</h2>
                     </div>
-                </div>
-                <h1>{t("about__skills")}</h1>
-                <div className="skills_desc">
+                </motion.div>
+                <motion.h1 initial={{ x: 50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>{t("about__skills")}</motion.h1>
+                <motion.div className="skills_desc" initial={{ x: 50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>
                     <h2>HTML5/CSS/JAVASCRIPT</h2>
                     <h2>REACTJS/NEXTJS/TYPESCRIPT</h2>
                     <h2>TAILWIND/BOOTSTRAP/SASS</h2>
                     <h2>GSAP/FRAMER MOTION</h2>
                     <h2>FIREBASE/GITHUB/GITLAB</h2>
-                </div>
+                </motion.div>
                 <MyStack />
                 <div className="kudos_desc">
-                <h1>KUDOS</h1>
-                    <h2>{t("about__kudos")}</h2>
+                    <motion.h1 initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>KUDOS</motion.h1>
+                    <motion.h2 initial={{ x: -50 }} whileInView={{ x: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}>{t("about__kudos")}</motion.h2>
                 </div>
-                <motion.img src={require("../../../../assets/profile2.JPG")} alt="profile" />
+                <motion.img src={require("../../../../assets/profile2.JPG")} alt="profile" initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] }}/>
             </div>
         </motion.div>
     )
