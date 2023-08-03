f97d43552e5ee4e8b0c51f007d45b15b7d4cbab7
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-03 16:03:08 +0400

    added the new changes for mobile. adjusted what's neccessary.

--- src/pages/portfolio/projectDetails/projectDetailsResponsive/mobileDetails/MobileDetails.jsx
+++ src/pages/portfolio/projectDetails/projectDetailsResponsive/mobileDetails/MobileDetails.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './MobileDetails.scss'
 import ScrollAnimation from '../../../../../animation/ScrollAnimation/ScrollAnimation';
-import { motion } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { BiRightArrowAlt } from 'react-icons/bi';
 import { IoChevronBack } from 'react-icons/io5';
+import { useRef } from 'react'
 
-
-const MobileDetails = ({ post, renderDetails, goToNextProject, nextTitleName, scrollYProgress, containerRef }) => {
+const MobileDetails = ({ post, renderDetails, goToNextProject, nextTitleName }) => {
+    const containerRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: containerRef,
+        offset: ["start end", "start start"]
+    });
     return (
         <>
             <ScrollAnimation />
-            <motion.div  ref={containerRef} className="project__details" style={{ width: "100vw", touchAction: "pan-y", }} initial={{ opacity: 0, y: -100, transition: { delay: 0.3, staggerChildren: 3.5, duration: 0.5, ease: [0.42, 0, 0.58, 1] } }} animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] } }} exit={{ opacity: 0, y: -500, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}>
+            <motion.div ref={containerRef} className="project__details" style={{ width: "100vw", touchAction: "pan-y", }} initial={{ opacity: 0, y: -100, transition: { delay: 0.3, staggerChildren: 3.5, duration: 0.5, ease: [0.42, 0, 0.58, 1] } }} animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] } }} exit={{ opacity: 0, y: -500, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}>
                 <div className="project__card_details">
                     <motion.div
                         className="background__img"
@@ -79,31 +84,44 @@ const MobileDetails = ({ post, renderDetails, goToNextProject, nextTitleName, sc
                         </div>
                         <div className="project__img_container" style={{ display: "flex", justifyContent: "center", alignContent: "center", alignItems: "center", flexDirection: "column", transform: "translateX(4rem)" }}>
                             {post.mainTitle && renderDetails(post.mainTitle, post.mainTitleImg, post.mainTitle)}
+                            {post.video && (
+                                <div className="project__details_details-mobile">
+                                    <div className="project__details-text-mobile">
+                                        <video src={post.video} muted autoPlay loop poster={post.image}></video>
+                                    </div>
+                                </div>
+                            )}
                             {post.mainPageOnHover && renderDetails(post.mainPageOnHover, post.mainPageOnHoverImg, post.mainPageOnHover)}
                             {post.mainTitle2 && renderDetails(post.mainTitle2, post.mainTitleImg2, post.mainTitle2)}
-                            {post.articleDetails && renderDetails(post.articleDetails, post.articleDetailsImg, post.articleDetails)}
-                            {post.editPage && renderDetails(post.editPage, post.editPageImg, post.editPage)}
-                            {post.createPost && renderDetails(post.createPost, post.createPostImg, post.createPost)}
                             {post.mainTitleSubMenu && renderDetails(post.mainTitleSubMenu, post.mainTitleSubMenuImg, post.mainTitleSubMenu)}
-                            {post.projectLogins && renderDetails(post.projectLogins, post.projectLoginsImg, post.projectLogins)}
-                            {post.registerPage && renderDetails(post.registerPage, post.registerPageImg, post.registerPage)}
                             {post.projetTestimonials && renderDetails(post.projetTestimonials, post.projetTestimonialsImg, post.projetTestimonials)}
                             {post.mainProjectsPage && renderDetails(post.mainProjectsPage, post.mainProjectsPageImg, post.mainProjectsPage)}
                             {post.accountPage && renderDetails(post.accountPage, post.accountPageImg, post.accountPage)}
                             {post.mainAboutPage && renderDetails(post.mainAboutPage, post.mainAboutPageImg, post.mainAboutPage)}
                             {post.mainContactPage && renderDetails(post.mainContactPage, post.mainContactPageImg, post.mainContactPage)}
+                            {post.mainWorkPage && renderDetails(post.mainWorkPage, post.mainWorkPageImg, post.mainWorkPage)}
                             {post.mainDarkMode && (
                                 <motion.div className="project__details_details">
                                     <div className="project__details-text">
                                         <h2>{post.mainDarkMode}</h2>
                                     </div>
-                                    <motion.div className="project__details-img" style={{ scale: scrollYProgress}}>
+                                    <motion.div className="project__details-img" style={{ scale: scrollYProgress }}>
                                         <img src={post.mainDarkModeImg} alt={post.mainContactPage} />
                                         <img src={post.mainDarkModeImg2} alt={post.mainContactPage} />
                                         <img src={post.mainDarkModeImg3} alt={post.mainContactPage} />
                                         <img src={post.mainDarkModeImg4} alt={post.mainContactPage} />
                                     </motion.div>
                                 </motion.div>
+                            )}
+                            {post.collective && (
+                                <div className="project__details_details-light-mobile">
+                                    <motion.div className="project__details-text-mobile">
+                                        <h2 style={{ width: window.innerWidth >= 860 ? "" : "100vw", }}>{post.overView}</h2>
+                                    </motion.div>
+                                    <div className="project__details-img-light-mobile">
+                                        <img src={post.collective} alt={post.mainContactPage} loading='lazy' />
+                                    </div>
+                                </div>
                             )}
                             {post.lightMode && (
                                 <div className="project__details_details">
