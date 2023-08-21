8b0180f80d77a5c65d9647c1a140b9a868389a36
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-21 05:37:23 +0300

    fixed the issue of language submenu wasn't clickable.

--- src/navItems/animatedNav/dropDownMenu/dropResponsive/mobileDrop/MobileDrop.jsx
+++ src/navItems/animatedNav/dropDownMenu/dropResponsive/mobileDrop/MobileDrop.jsx
@@ -5,34 +5,32 @@ import { AnimatePresence, motion } from 'framer-motion'
 const MobileDrop = ({ navOpen, language, handleLanguageExpanded, languageExpanded, handleEnglishClick, handleGermanClick, handleFrenchClick, handleArabicClick }) => {
     return (
         <div className={`${navOpen ? "dropdown-menu_mobile" : "dropdown-menu_mobile"}`}>
-            <div
-                onClick={handleLanguageExpanded}
-                className={navOpen ? 'dropdown__icon_mobile spin' : 'dropdown__icon_mobile'}
-                style={{ zIndex: 999999999, position: "relative" }}>
+            <div onClick={handleLanguageExpanded} className={navOpen ? 'dropdown__icon_mobile spin' : 'dropdown__icon_mobile'}
+            >
+
                 <span onClick={handleLanguageExpanded}>
                     {language}
                     <FaAngleDown />
+                </span>
                     <AnimatePresence>
                         {languageExpanded && (
                             <motion.div initial={{ x: 15, opacity: 0 }} animate={{ x: 3, opacity: 1 }} exit={{ x: 15, opacity: 0 }} className="language__dropdown-mobile"
-                                style={{ top: navOpen ? "2.5rem" : "4rem", right: navOpen ? language === "ar" ? "13rem" : "-1rem" : language === "ar" ? "-6rem" : "-3rem", width: "14rem", zIndex: 999999999, position: "relative"}}>
-                                <div className="language__dropdown-link" onClick={handleEnglishClick}>
-                                    <span className="language__text">en</span>
+                                style={{ top: navOpen ? "2.5rem" : "4rem", right: navOpen ? language === "ar" ? "0rem" : "-1rem" : language === "ar" ? "-6rem" : "-3rem", width: "16rem"}}>
+                                <div className="language__dropdown-link_mobile" onClick={handleEnglishClick}>
+                                    <span className="language__text_mobile">en</span>
                                 </div>
-                                <div className="language__dropdown-link" onClick={handleGermanClick}>
-                                    <span className="language__text">de</span>
+                                <div className="language__dropdown-link_mobile" onClick={handleGermanClick}>
+                                    <span className="language__text_mobile">de</span>
                                 </div>
-                                <div className="language__dropdown-link" onClick={handleFrenchClick}>
-                                    <span className="language__text">fr</span>
+                                <div className="language__dropdown-link_mobile" onClick={handleFrenchClick}>
+                                    <span className="language__text_mobile">fr</span>
                                 </div>
-                                <div className="language__dropdown-link" onClick={handleArabicClick}>
-                                    <span className="language__text">ar</span>
+                                <div className="language__dropdown-link_mobile" onClick={handleArabicClick}>
+                                    <span className="language__text_mobile">ar</span>
                                 </div>
                             </motion.div>
                         )}
                     </AnimatePresence>
-                </span>
-                <div className={navOpen ? 'language__dash spin' : 'language__dash'}></div>
             </div>
         </div>
     )
