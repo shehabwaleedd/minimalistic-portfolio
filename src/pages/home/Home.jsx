bf4b23c1497b61a59d3e3ab70ecd2c556b41a7cb
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-02 10:38:48 +0300

    added meta tags for home page.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import Main from './main/Main';
 import './Home.css';
 import { motion } from 'framer-motion';
+import { Helmet } from 'react-helmet-async';
 
 const Home = ({ isMobile, navOpen, language, isTablet }) => {
   return (
     <motion.section className="homeee" >
+      <Helmet>
+        <title>Shehab Waleed - Frontend Developer & Freelancer</title>
+        <meta
+          name="description"
+          content="Transform your vision into reality with Shehab Waleed, a passionate frontend developer and freelancer. From stunning designs to seamless development, I bring your ideas to life."
+        />
+        <link rel="canonical" href="/" />
+      </Helmet>
       <div className="main__containerr">
         <Main isMobile={isMobile} isTablet={isTablet} navOpen={navOpen} language={language} />
       </div>
